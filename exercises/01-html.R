48056ca7ca7b76838398bf11ef06a09dea896747
unknown <loewesic@SOWI.HU-BERLIN.DE>
2026-04-25 12:03:24 +0200

    Update 01-html.R

--- exercises/01-html.R
+++ exercises/01-html.R
@@ -13,13 +13,28 @@
 # 6. Load the page into R
 
 # 7. Use the rvest package and the css path from above to extract all elements, and store them in an object.
+nodes_01 <- read_html("https://www.aph.gov.au/Senators_and_Members/Parliamentarian_Search_Results?&q=&mem=1&par=-1&gen=0&ps=96&st=1") %>% 
+  html_nodes(".title a")
 
 # 8. Get the href attribute for all elements
 
+html_attr(nodes_01, name = "href")
+
 # 9. Get the text for all elements
+
+html_text2(nodes_01)
 
 # 10. Create a data.frame with two columns, the link/href from above and the the name/text from above. Store the data.frame in an object.
 
+df01 <- data.frame(name = html_text(nodes_01),
+                   str_c("https://www.aph.gov.au", html_attr(nodes_01, name = "href")))
+
 # 11. Try finding a CSS path for the party of the members on the page. (It might not be possible.)
+parties <- read_html("https://www.aph.gov.au/Senators_and_Members/Parliamentarian_Search_Results?&q=&mem=1&par=-1&gen=0&ps=96&st=1") |> 
+  html_nodes(xpath = "//dt[contains(text(),'Party')]/following-sibling::dd[1]") |> 
+  html_text(trim = TRUE)
 
 # 12. How would you add the party of members to your dataset?
+df01 <- data.frame(name = html_text(nodes_01),
+                   url = str_c("https://www.aph.gov.au", html_attr(nodes_01, name = "href")),
+                   party = parties)
