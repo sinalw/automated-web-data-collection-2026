42900574b62a809c55393506d813205c7a0bb953
unknown <loewesic@SOWI.HU-BERLIN.DE>
2026-04-25 11:59:23 +0200

    Exercise 02

--- exercises/02-webscraping.R
+++ exercises/02-webscraping.R
@@ -16,24 +16,82 @@ read_html(destfile)
 
 # 2. Create a vector with links to all the pages with links to the questions/responses.
 
+pages <- str_c("https://www.abgeordnetenwatch.de/berlin/fragen-antworten?page=", 0:235)
+pages
+
 # 3. Create a folder where you will save the HTML files with the full questions/responses.
 
+setwd("C:/Users/loewesic/Nextcloud/0_DYNAMICS/DYNAMICS_Courses/Automated Web Data Collection")
+if(!dir.exists("exercise02")) dir.create("exercise02")
+
 # 4. Read the first page into R, and save it in the created folder.
+
+filename <- str_c("exercise02/page_1.html")
+print(filename)
+myhtml <- read_html(pages[1])
+write_html(myhtml, file = filename)
 
 # 5. Build a for loop for the links to the first three pages. First define a filename that is different for each link.
 # (hint: you can name the files "0.html", "1.html", etc. by extracting the page number from the link via regular expression).
 # Read the html from the link, then write it to the filename.
 # Make sure to pause R at the end of each iteration.
+library(rvest)
+library(xml2)
+
+for (i in 1:3) {
+  page <- pages[i]
+  filename <- str_c("exercise02/page_", i, ".html")
+  if(file.exists(filename)) next 
+  print(filename)
+  myhtml <- read_html(page)
+  write_html(myhtml, file = filename)
+  Sys.sleep(1)
+}
 
 # 6. Read the first html page (from the folder) and extract all links to the page for the full question/response on that page using a css path (hint: ".tile__question__teaser a").
 
+myhtml <- read_html("exercise02/page_1.html")
+
+myelements <- html_elements(myhtml, "article div.question__question a")
+
+myelements
+
+links <- html_attr(myelements, "href")
+
+links
+
 # 7. Create a vector with the (full) file path to each file in your folder. (hint: The path has to include the folder name.)
+
+files <- list.files("exercise02/", full.names = T)
+
+files
 
 # 8. Build a for loop for all files in your folder. Extract the links and add them all to a vector.
 
+links <- c()
+for (file in files) {
+  print(file)
+  
+  myhtml <- read_html(file)
+  
+  myelements <- html_elements(myhtml, "article div.question__question a")
+  
+  links <- c(links, html_attr(myelements, "href")) # Extract the links and add them to the links vector
+  
+}
+
+links
+
+
 # 9. The links are only relative URLs, i.e. they are missing the domain. Add the domain using a stringr function.
 
+links <- str_c("https://www.abgeordnetenwatch.de/", links)
+
 # 10. Download the first three HTMLs with the full questions/responses. (hint: download.file(url, destfile)
+for (link in links[1:3]) {
+  download.file(link, destfile = str_c("exercise02/", basename(link), ".html"))
+}
+
 
 i <- 100
 for (i in 0:2) {
@@ -47,7 +105,6 @@ i
 function(){
   i <- 200
 }
-
 
 
 # Load required packages
